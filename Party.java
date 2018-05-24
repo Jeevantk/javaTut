f4f329c261b4fc46ef58e861c0ab627c9394d128
jeevantk <jeevan.thomaskoshy@gmail.com>
2018-05-24 09:49:53 +0530

    Added First tut

---
+++ Party.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+
+class Party{
+	public void buildInvite(){
+		Frame f = new Frame();
+		Label l = new Label("Party at Tim's");
+		Button b = new Button("You bet");
+		Button c = new Button("Shoot me");
+		Panel p= new Panel();
+		p.add(l);
+	}
+
+	public static void main(String[] args){
+		System.out.println("I Rule!");
+		double d= Math.random();
+		System.out.println(d);
+	}
+}
